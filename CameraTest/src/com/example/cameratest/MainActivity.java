b678b539e8c9b3841acefdadbdcb70b505be3c78
Steven Cherfan <cherfan@ualberta.ca>
2014-10-07 13:01:24 -0600

    Random message commit

--- CameraTest/src/com/example/cameratest/MainActivity.java
+++ CameraTest/src/com/example/cameratest/MainActivity.java
@@ -1,3 +1,4 @@
+
 package com.example.cameratest;
 
 import java.io.File;
@@ -15,18 +16,23 @@ import android.view.View.OnClickListener;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity
+{
 
 	Uri imageFileUri;
 
 	@Override
-	public void onCreate(Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState)
+	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
 		ImageButton button = (ImageButton) findViewById(R.id.TakeAPhoto);
-		OnClickListener listener = new OnClickListener() {
-			public void onClick(View v) {
+		OnClickListener listener = new OnClickListener()
+		{
+
+			public void onClick(View v)
+			{
 				takeAPhoto();
 			}
 		};
@@ -34,17 +40,21 @@ public class MainActivity extends Activity {
 	}
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
+	public boolean onCreateOptionsMenu(Menu menu)
+	{
 		getMenuInflater().inflate(R.menu.activity_main, menu);
 		return true;
 	}
 
 	private static final int CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE = 100;
 
-	public void takeAPhoto() {
+	public void takeAPhoto()
+	{
 		// TODO: Create an intent with the action
 		// MediaStore.ACTION_IMAGE_CAPTURE
-		
+
+		Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
 		// ComponentName cn = new ComponentName("es.softwareprocess.bogopicgen",
 		// "es.softwareprocess.bogopicgen.BogoPicGenActivity");
 		// ComponentName cn = new ComponentName("com.android.camera",
@@ -53,35 +63,55 @@ public class MainActivity extends Activity {
 
 		// Create a folder to store pictures
 		String folder = Environment.getExternalStorageDirectory()
-				.getAbsolutePath() + "/tmp";
+		        .getAbsolutePath() + "/tmp";
 		File folderF = new File(folder);
-		if (!folderF.exists()) {
+		if (!folderF.exists())
+		{
 			folderF.mkdir();
 		}
 
 		// Create an URI for the picture file
 		String imageFilePath = folder + "/"
-				+ String.valueOf(System.currentTimeMillis()) + ".jpg";
+		        + String.valueOf(System.currentTimeMillis()) + ".jpg";
 		File imageFile = new File(imageFilePath);
 		imageFileUri = Uri.fromFile(imageFile);
 
 		// TODO: Put in the intent in the tag MediaStore.EXTRA_OUTPUT the URI
-		
+		intent.putExtra(MediaStore.EXTRA_OUTPUT, imageFileUri);
+
 		// TODO: Start the activity (expecting a result), with the code
 		// CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE
-		
+		startActivityForResult(intent, CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE);
 	}
 
-	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+	protected void onActivityResult(int requestCode, int resultCode, Intent data)
+	{
 		// TODO: Handle the results from CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE
-		
+
 		// TODO: Handle the cases for RESULT_OK, RESULT_CANCELLED, and others
-		
+
 		// When the result is OK, set text "Photo OK!" in the status
-		//		and set the image in the Button with:
-		//		button.setImageDrawable(Drawable.createFromPath(imageFileUri.getPath()));
+		// and set the image in the Button with:
+		// button.setImageDrawable(Drawable.createFromPath(imageFileUri.getPath()));
 		// When the result is CANCELLED, set text "Photo canceled" in the status
 		// Otherwise, set text "Not sure what happened!" with the resultCode
-		
+		if (requestCode == CAPTURE_IMAGE_ACTIVITY_REQUEST_CODE)
+		{
+			TextView tv = (TextView) findViewById(R.id.status);
+			
+			if (resultCode == RESULT_OK)
+			{
+				String message = data.getStringExtra("Mess");
+				tv.setText(message);
+				//tv.setText("Photo OK!");
+				
+				ImageButton button = (ImageButton) findViewById(R.id.TakeAPhoto);
+				button.setImageDrawable(Drawable.createFromPath(imageFileUri.getPath()));
+			}
+			else if(resultCode == RESULT_CANCELED)
+				tv.setText("Photo canceled!");
+			else
+				tv.setText("Dunno what happened");
+		}
 	}
 }
